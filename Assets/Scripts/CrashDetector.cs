be9a8b54d2a011f542b0be981c763d976ea6c3ea
Nicolas Panozo <nicopanozoc@gmail.com>
2023-06-14 10:22:38 -0400

    juego terminado y con menu tambien sonidos de crash y finish

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -9,11 +9,18 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.8f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
+    bool hasCrashed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Ground"))
+        if (other.CompareTag("Ground") && !hasCrashed)
         {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
